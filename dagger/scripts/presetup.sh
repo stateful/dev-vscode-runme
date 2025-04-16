2c48ad1c02d8db0da845f0ba3341e88c6094c179
Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>
2025-04-16 13:23:01 -0700

    Add virtual frame buffer
    Signed-off-by: Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>

--- dagger/scripts/presetup.sh
+++ dagger/scripts/presetup.sh
@@ -2,4 +2,4 @@ mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/g
 && chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && apt update \
-&& apt install gh -y
+&& apt install gh xvfb -y
